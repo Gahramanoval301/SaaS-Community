619426bbba379589f2303566ebb575ee171d904e
Gahramanoval301 <lemangahramanoval@gmail.com>
2023-12-13 15:15:29 +0300

    create theme and add palette property

---
+++ src/theme.js
@@ -0,0 +1,13 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#000',
+        },
+        secondary: {
+            main: '#6B7CFF',
+        },
+    },
+})
+export default theme;
