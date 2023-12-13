280eca1c06800b65c29ef0c1e0ee83217cce45d4
Gahramanoval301 <lemangahramanoval@gmail.com>
2023-12-13 15:17:31 +0300

    mockData - sections

---
+++ src/mockData/sections.js
@@ -0,0 +1,25 @@
+export const sections = [{
+    id: 0,
+    title: 'Home',
+    href: '/',
+}, {
+    id: 1,
+    title: 'Product',
+    href: '/product',
+}, {
+    id: 2,
+    title: 'Pricing',
+    href: '/pricing',
+}, {
+    id: 3,
+    title: 'About Us',
+    href: '/about-us',
+}, {
+    id: 4,
+    title: 'Blog',
+    href: '/blog',
+}, {
+    id: 5,
+    title: 'Contact',
+    href: '/contact',
+}]
