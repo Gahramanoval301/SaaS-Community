71c562bb8751e395cf1d4bcbaabef0c3342bd327
Gahramanoval301 <lemangahramanoval@gmail.com>
2023-12-13 18:22:20 +0300

    add dropdown for responsive design to header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,19 +6,31 @@ import Toolbar from '@mui/material/Toolbar';
 import styles from './index.module.css';
 import ButtonC from '../Button';
 import { sections } from '../../mockData/sections';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import MenuIcon from '@mui/icons-material/Menu';
+import { SafetyCheckSharp } from '@mui/icons-material';
 
 const Header = () => {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
     return (
         <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static" sx={{ backgroundColor: 'transparent' }} elevation={0}>
+            <AppBar position="static" sx={{ backgroundColor: 'white', py: 1, position:'fixed'}} elevation={0}>
                 <Toolbar>
                     <Container >
-                        <Stack flexDirection={'row'} justifyContent={'space-between'} alignItems={'center'}>
+                        <Stack flexDirection={'row'} justifyContent={'space-between'} alignItems={'center'} flexWrap={'wrap'}>
                             <Box>
                                 <img src="/Logo.png" alt="Logo" />
                             </Box>
                             <Box>
-                                <Box>
+                                <Box className={styles.navbar}>
                                     <nav style={{ display: 'flex', alignItems: 'center', gap: '16px' }}>
                                         <Stack flexDirection={'row'} sx={{ gap: 2 }}>
                                             {sections.map(({ id, title, href }) => {
@@ -27,6 +39,43 @@ const Header = () => {
                                                 )
                                             })}
                                         </Stack>
+                                        <ButtonC title={'Free trial'} />
+                                    </nav>
+                                </Box>
+                                <Box className={styles.responsiveMenu}>
+                                    <nav className={styles.responsiveNav} style={{ display: 'flex', alignItems: 'center' ,gap: '16px'}}>
+                                        <div>
+                                            <Button
+                                                id="basic-button"
+                                                aria-controls={open ? 'basic-menu' : undefined}
+                                                aria-haspopup="true"
+                                                aria-expanded={open ? 'true' : undefined}
+                                                onClick={handleClick}
+                                            >
+                                                <MenuIcon fontSize='large' />
+
+                                            </Button>
+                                            <Menu
+                                                id="basic-menu"
+                                                anchorEl={anchorEl}
+                                                open={open}
+                                                onClose={handleClose}
+                                                MenuListProps={{
+                                                    'aria-labelledby': 'basic-button',
+                                                }}
+                                            >
+                                                <Stack className={styles.menuList}>
+
+                                                    {sections.map(({ id, title, href }) => {
+                                                        return (
+                                                            <MenuItem onClick={handleClose} >
+                                                                <Link to={href} underline="none" key={id} className={styles.link}>{title}</Link>
+                                                            </MenuItem>
+                                                        )
+                                                    })}
+                                                </Stack>
+                                            </Menu>
+                                        </div>
                                         <ButtonC title={'Free trial'} />
                                     </nav>
                                 </Box>
