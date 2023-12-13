b66baaf838cbe7982c55ea08da07aa2be3de5340
Gahramanoval301 <lemangahramanoval@gmail.com>
2023-12-13 15:15:39 +0300

    create Button component

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Button } from '@mui/material'
+
+const ButtonC = ({title}) => {
+  return (
+    <Button sx={{backgroundColor:'primary.main', color:'white',fontFamily:'inherit', textTransform:'none', padding:'16px 35px', borderRadius:'12px',fontWeight:600, '&:hover':{backgroundColor:'secondary.main'}}}>{title}</Button>
+    )
+}
+
+export default ButtonC
