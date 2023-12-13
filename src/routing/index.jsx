f85e8127b061a664618df035e153ce2a1b4b3ea2
Gahramanoval301 <lemangahramanoval@gmail.com>
2023-12-13 13:44:27 +0300

    Create Webrouting files and add some routes

---
+++ src/routing/index.jsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router"
+import Home from "../pages/Home"
+import About from "../pages/About"
+const WebRouting = () => {
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </>
+    )
+}
+export default WebRouting
