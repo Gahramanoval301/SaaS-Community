73174bdb58ee839b955d0022b913a533937b981b
Gahramanoval301 <lemangahramanoval@gmail.com>
2023-12-15 00:14:06 +0300

    add few sections to Home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 import PageContainer from '../../components/PageContainer'
 import Starting from '../../components/HomeComps/Starting'
 import FeatuesH from '../../components/HomeComps/FeaturesH'
+import WhyUsH from '../../components/HomeComps/WhyUs'
+import CTAhome from '../../components/HomeComps/CTAhome'
+import Logos from '../../components/LogosH'
+import DiscoverMore from '../../components/HomeComps/DiscoverMoreH'
 
 const Home = () => {
   return (
     <PageContainer>
       <Starting />
-      <FeatuesH/>
+      <FeatuesH />
+      <WhyUsH />
+      <CTAhome />
+      <Logos />
+      <DiscoverMore />
     </PageContainer>
   )
 }
