8a2eb6ded5bbf1dff6dd4c3616ccdf12a24bc9a6
Gahramanoval301 <lemangahramanoval@gmail.com>
2023-12-15 00:17:10 +0300

    some corrections in FeaturesH section of Home page

--- src/components/HomeComps/FeaturesH/index.jsx
+++ src/components/HomeComps/FeaturesH/index.jsx
@@ -9,16 +9,19 @@ import styles from './index.module.css'
 
 const FeatuesH = () => {
   return (
-    <Container sx={{ border: '1px solid red', py: 3 }}>
+    <Container sx={{ py: 3 }}>
       <Stack spacing={2} alignItems={'center'} justifyContent={'center'}>
-        <Typography variant="body1" className={styles.textGradient}>High-quality</Typography>
-        <Typography variant="h3" color="initial" sx={{ fontWeight: 600, width:{xs:'100%',sm:'80%', md:'65%'}, fontSize: { xs: '2.3rem', md: '3rem' },textAlign:'center'}}>We have the Best Solution for your Business</Typography>
-        <Grid container spacing={2} sx={{ p:{xs:0,sm:3}, backgroundColor:'#E0E4FC', borderRadius:'24px'}}>
+        <Typography variant="h6" sx={{fontWeight:600,
+          backgroundImage: 'linear-gradient(to right, #373FFF, #3ACAF8)',
+          WebkitBackgroundClip: 'text', color: ' transparent', backgroundClip: 'text'
+        }}>High-quality</Typography>
+        <Typography variant="h3" color="initial" sx={{ fontWeight: 600, width: { xs: '100%', sm: '80%', md: '65%' }, fontSize: { xs: '2.3rem', md: '3rem' }, textAlign: 'center' }}>We have the Best Solution for your Business</Typography>
+        <Grid container spacing={2} sx={{ p: { xs: 0, sm: 3 }, backgroundColor: '#E0E4FC', borderRadius: '24px' }}>
           <Grid item sm={6} md={4}>
-            <Card sx={{  backgroundColor:'transparent' }} elevation={0}>
+            <Card sx={{ backgroundColor: 'transparent' }} elevation={0}>
               <CardContent>
                 <VerifiedUserIcon sx={{ fontSize: '45px', color: 'secondary.main' }} />
-                <Typography gutterBottom variant='h5' fontWeight={600} sx={{minHeight:{sm:'100px',md:'65px'}}}>
+                <Typography gutterBottom variant='h5' fontWeight={600} sx={{ minHeight: { sm: '100px', md: '65px' } }}>
                   High security to protect from piracy
                 </Typography>
                 <Typography variant="body1" color="initial">
@@ -28,10 +31,10 @@ const FeatuesH = () => {
             </Card>
           </Grid>
           <Grid item sm={6} md={4}>
-            <Card sx={{  backgroundColor:'transparent' }} elevation={0}>
+            <Card sx={{ backgroundColor: 'transparent' }} elevation={0}>
               <CardContent>
                 <ThumbUpAltRoundedIcon sx={{ fontSize: '45px', color: 'secondary.main' }} />
-                <Typography gutterBottom variant='h5' fontWeight={600} sx={{minHeight:{sm:'100px',md:'65px'}}}>
+                <Typography gutterBottom variant='h5' fontWeight={600} sx={{ minHeight: { sm: '100px', md: '65px' } }}>
                   Premium quality performance
                 </Typography>
                 <Typography variant="body1" color="initial">
@@ -41,10 +44,10 @@ const FeatuesH = () => {
             </Card>
           </Grid>
           <Grid item sm={6} md={4}>
-            <Card sx={{ backgroundColor:'transparent' }} elevation={0}>
+            <Card sx={{ backgroundColor: 'transparent' }} elevation={0}>
               <CardContent>
                 <HeadsetMicRoundedIcon sx={{ fontSize: '45px', color: 'secondary.main' }} />
-                <Typography gutterBottom variant='h5' fontWeight={600} sx={{minHeight:{sm:'100px',md:'65px'}}}>
+                <Typography gutterBottom variant='h5' fontWeight={600} sx={{ minHeight: { sm: '100px', md: '65px' } }}>
                   Full time customer support - 24/7
                 </Typography>
                 <Typography variant="body1" color="initial">
