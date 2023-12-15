5b108d443fd4f62ebdd0260590f320df28575d3b
Gahramanoval301 <lemangahramanoval@gmail.com>
2023-12-15 22:46:09 +0300

    datas of testimonials

---
+++ src/components/HomeComps/Testimonials/data-testimonials.js
@@ -0,0 +1,21 @@
+export const testimonials = [{
+    id: 0,
+    name: 'Ron Wood',
+    comment: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.',
+    job: 'CEO',
+    img: '/HomeSources/man1.png'
+},
+{
+    id: 1,
+    name: 'Mark Mason',
+    comment: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.',
+    job: 'Marketing Manager',
+    img: '/HomeSources/man2.png'
+},
+{
+    id: 2,
+    name: 'Sam Preston',
+    comment: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.',
+    job: 'CTO',
+    img: '/HomeSources/man3.png'
+}]
