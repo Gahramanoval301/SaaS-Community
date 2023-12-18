ad1d0500428321b38e90c174a311cfb1bf9a1b03
Gahramanoval301 <lemangahramanoval@gmail.com>
2023-12-18 21:56:19 +0300

    footerSections' titles, subSections and their titles and links

---
+++ src/components/Footer/footerSections.jsx
@@ -0,0 +1,58 @@
+export const footerSections = [
+    {
+        id: 0,
+        title: 'Compony',
+        sections: [
+            {
+                title: 'About Us',
+                to: '/about-us'
+            },
+            { title: 'Why Choose us' },
+            {
+                title: 'Pricing',
+                to: '/pricing'
+            },
+            {
+                title: 'Testimonial',
+            }
+        ]
+    }, {
+        id: 1,
+        title: 'Resources',
+        sections: [
+            { title: 'Privary Policy' },
+            { title: 'Terms and Conditions' },
+            { title: 'Blog' },
+            {
+                title: 'Contact Us',
+                to: '/contact'
+            }
+        ]
+    }, {
+        id: 2,
+        title: 'Product',
+        sections: [
+            {
+                title: 'Project Management',
+                to: '/product'
+            },
+            {
+                title: 'Time cracker',
+                to: '/product'
+            },
+            {
+                title: 'Time schedule',
+                to: '/product'
+            },
+            {
+                title: 'Lead Generate',
+                to: '/product'
+            },
+            {
+                title: 'Remote Colloboration',
+                to: '/product'
+            }
+        ]
+    }
+
+]
