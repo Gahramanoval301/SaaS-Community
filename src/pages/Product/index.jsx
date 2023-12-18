d86beb568667154810a3c8c323b0a4a439b76663
Gahramanoval301 <lemangahramanoval@gmail.com>
2023-12-18 21:09:28 +0300

    the structure of Products Page

--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import Starting from '../../components/ProductComps/Starting'
 import PageContainer from '../../components/PageContainer'
-import Features from '../../components/ProductComps/Features'
+import FeaturesP from '../../components/ProductComps/Features'
+import MarketingInsights from '../../components/ProductComps/MarketingInsight'
+import TimeCracker from '../../components/ProductComps/TimeCracker'
+import LeadGeneration from '../../components/ProductComps/LeadGeneration'
+import RemoteTeams from '../../components/ProductComps/RemoteTeams'
+import Features from '../../components/Features'
 
 const Product = () => {
     return (
         <PageContainer>
             <Starting />
+            <FeaturesP/>
+            <MarketingInsights/>
+            <TimeCracker/>
+            <LeadGeneration/>
+            <RemoteTeams/>
             <Features/>
         </PageContainer>
     )
