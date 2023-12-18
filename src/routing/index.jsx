96dd5cd53ba260d2cde0207f31df7bc17fc511d7
Gahramanoval301 <lemangahramanoval@gmail.com>
2023-12-18 21:22:46 +0300

    add routings of pages

--- src/routing/index.jsx
+++ src/routing/index.jsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router"
 import Home from "../pages/Home"
 import About from "../pages/About"
 import Product from "../pages/Product"
+import Pricing from "../pages/Pricing"
+import Contact from "../pages/Contact"
 
 const WebRouting = () => {
     return (
@@ -9,6 +11,9 @@ const WebRouting = () => {
                 <Route path="/" element={<Home />} />
                 <Route path="/product" element={<Product />} />
                 <Route path="/about-us" element={<About />} />
+                <Route path="/pricing" element={<Pricing />} />
+                <Route path="/contact" element={<Contact />} />
+
             </Routes>
     )
 }
