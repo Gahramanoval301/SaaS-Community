3c1b503d4dad2b6c373a8effedba470eb7111722
Gahramanoval301 <lemangahramanoval@gmail.com>
2023-12-18 16:39:14 +0300

    correction of letter

--- src/theme.js
+++ src/theme.js
@@ -4,7 +4,7 @@ const theme = createTheme({
     palette: {
         primary: {
             main: '#000',
-            ligth:'#fff'
+            light:'white'
         },
         secondary: {
             main: '#6B7CFF',
