f66899f00dbf629b94111d5c996ae0f2d7286b87
Gahramanoval301 <lemangahramanoval@gmail.com>
2023-12-21 00:48:55 +0300

    data of FAQ items for FAQ section of Pricing Page

---
+++ src/components/PricingComps/FAQ/data.jsx
@@ -0,0 +1,17 @@
+export const data = [{
+    id: 0,
+    question: 'How do I grow my business?',
+    answer: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.'
+}, {
+    id: 1,
+    question: 'Can I cancel my subscription?',
+    answer: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.'
+}, {
+    id: 2,
+    question: 'How do I contact the support?',
+    answer: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.'
+}, {
+    id: 3,
+    question: 'Is a credit card required?',
+    answer: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.'
+}]
