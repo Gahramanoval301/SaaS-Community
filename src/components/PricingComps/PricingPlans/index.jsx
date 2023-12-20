16ef11bd40a5ed14b60b3ff4f74dfdbb4842d23f
Gahramanoval301 <lemangahramanoval@gmail.com>
2023-12-20 22:29:02 +0300

    the structure and designing of PricingPlans section of Pricing page

--- src/components/PricingComps/PricingPlans/index.jsx
+++ src/components/PricingComps/PricingPlans/index.jsx
@@ -1,32 +1,59 @@
 import React from 'react'
 import { Typography, Box, Stack, Container, Grid, Button } from '@mui/material'
+import { useState } from 'react'
 import styles from './index.module.css'
+import { data } from './dataCards'
+import PricingCard from './PricingCard'
 
 const PricingPlans = () => {
+    const [isToggled, setIsToggled] = useState(JSON.parse(localStorage.getItem('isToggled')))
+    localStorage.setItem('isToggled', JSON.stringify(isToggled))
+    console.log(JSON.parse(localStorage.getItem('isToggled')))
+
+    const style = {
+        backgroundColor: isToggled ? 'black' : '',
+        color: isToggled ? 'white' : '',
+    };
+
+    const style2 = {
+        backgroundColor: (!isToggled) ? 'black' : '',
+        color: (!isToggled) ? 'white' : '',
+    };
+
+    function handleClick() {
+        setIsToggled(!JSON.parse(localStorage.getItem('isToggled')))
+        console.log(isToggled);
+    }
     return (
-        <Container>
-            <Stack spacing={3} sx={{ border: '1px solid red' }}>
+        <Container sx={{ py: 5 }}>
+            <Stack spacing={5} sx={{ border: '1px solid red' }}>
                 <Stack spacing={2} justifyContent={'center'} alignItems={'center'}>
-                    <Typography variant="h3" sx={{ fontWeight: 600 }}>
-                        Pricing plans that suit you
-                    </Typography>
-                    <Typography variant="body1">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.
-                    </Typography>
-                    <Box sx={{ backgroundColor: '#E0E4FC', borderRadius: '20px', padding: 1 }}>
-                        <Button sx={{ borderRadius: '12px', backgroundColor: 'primary.main', color: 'primary.light', padding: '14px 45px', textTransform: 'none' }}>Monthly</Button>
-                        <Button sx={{ padding: '14px 45px', borderRadius: '12px', textTransform: 'none' }}>Yearly</Button>
+                    <Box sx={{ width: { xs: '100%', sm: '70%', md: '40%' }, textAlign: 'center' }}>
+                        <Typography variant="h3" sx={{
+                            fontWeight: 600,
+                            fontSize: { xs: '2rem', sm: '2.5rem', md: '3rem' }, mb: 1
+                        }}>
+                            Pricing plans that suit you
+                        </Typography>
+                        <Typography variant="body1">
+                            Lorem ipsum dolor sit amet,
+                            consectetur adipiscing elit, sed do eiusmod tempor.
+                        </Typography>
+                    </Box>
+                    <Box onClick={handleClick} className={styles.btn_group} >
+                        <Button className={styles.btn} style={style}>Monthly</Button>
+                        <Button className={styles.btn} style={style2}>Yearly</Button>
                     </Box>
                 </Stack>
                 <Stack>
-                    <Grid container>
-                        <Grid item>
-                            
-                        </Grid>
+                    <Grid container spacing={2}>
+                        {
+                            data.map(data => <PricingCard data={data} key={data.id} />
+                            )}
                     </Grid>
                 </Stack>
             </Stack>
-        </Container>
+        </Container >
     )
 }
 
