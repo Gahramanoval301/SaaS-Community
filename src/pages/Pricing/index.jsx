81678f3fd09f35a7dbc4b3b8a431f3096e5cacbc
Gahramanoval301 <lemangahramanoval@gmail.com>
2023-12-21 00:49:12 +0300

    add FAQ section to Pricing Page

--- src/pages/Pricing/index.jsx
+++ src/pages/Pricing/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import PageContainer from '../../components/PageContainer'
 import PricingPlans from '../../components/PricingComps/PricingPlans'
+import FAQ from '../../components/PricingComps/FAQ'
 const Pricing = () => {
   return (
     <PageContainer>
       <PricingPlans />
+      <FAQ/>
     </PageContainer>
 
   )
