ab13f3b394c1f8e6152ee6c700727491fc150363
Gahramanoval301 <lemangahramanoval@gmail.com>
2023-12-20 22:31:00 +0300

    data of pricing plans' cards

---
+++ src/components/PricingComps/PricingPlans/dataCards.jsx
@@ -0,0 +1,27 @@
+
+
+export const data = [{
+    id: 0,
+    headerTitle: 'Regular',
+    headerBody: 'Starter Plan',
+    plans: ['Limited Projects', 'Regular Support Business', '1 month Free Trial', '3GB storage', 'Ads Preview'],
+    body: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr.',
+    price: 'Free',
+    expireDate: 'For Limited Period'
+}, {
+    id: 1,
+    headerTitle: 'Plantium',
+    headerBody: 'For the best results',
+    plans: ['Limited Projects', 'Regular Support Business', '1 month Free Trial', '3GB storage', 'Ads Preview'],
+    body: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr.',
+    price: '$342',
+    expireDate: 'For Limited Period'
+}, {
+    id: 2,
+    headerTitle: 'Standard',
+    headerBody: 'Most popular',
+    plans: ['Limited Projects', 'Regular Support Business', '1 month Free Trial', '3GB storage', 'Ads Preview'],
+    body: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr.',
+    price: '$234',
+    expireDate: 'Billing Monthly'
+}]
