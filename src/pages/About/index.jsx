9287d94e478f102391068eec334916adfb8bcf81
Gahramanoval301 <lemangahramanoval@gmail.com>
2023-12-24 16:28:22 +0300

    full sections of about-us page

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import PageContainer from '../../components/PageContainer'
 import HeaderA from '../../components/AboutUsComps/Header'
+import GoalsAndVisions from '../../components/AboutUsComps/GoalsAnsVisions'
+import Values from '../../components/AboutUsComps/Values'
+import Team from '../../components/AboutUsComps/Team'
 
 const About = () => {
   return (
     <PageContainer>
       <HeaderA />
+      <GoalsAndVisions />
+      <Values />
+      <Team />
     </PageContainer>
   )
 }
