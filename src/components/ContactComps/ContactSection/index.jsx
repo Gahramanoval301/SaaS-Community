5bd2d2b96b911583da7f04a72142d64eb6799457
Gahramanoval301 <lemangahramanoval@gmail.com>
2023-12-24 11:55:44 +0300

    optimizations and add radial-gradient to contactSection

--- src/components/ContactComps/ContactSection/index.jsx
+++ src/components/ContactComps/ContactSection/index.jsx
@@ -6,48 +6,50 @@ import ContactForm from '../ContactForm';
 
 const ContactSection = () => {
     return (
-        <Container sx={{ py: { xs: 4, md: 8 } }}>
-            <Stack spacing={5} >
-                <Typography variant="h3" sx={{ textAlign: 'center' }}>
-                    Get in touch with us
-                </Typography>
-                <Grid container spacing={2} sx={{
-                    bgcolor: '#F0F2FE',
-                    borderRadius: '24px',
-                    padding: { xs: 1, sm: 5 }
-                }}>
-                    <Grid item xs={12} md={6}>
-                        <ContactForm />
-                    </Grid>
-                    <Grid item xs={12} md={6} >
-                        <Stack justifyContent={'center'} alignItems={'center'} spacing={2}
-                            sx={{ height: '100%', p: 1 }}>
-                            <Stack spacing={2} className={styles.contactContainer}>
-                                {
-                                    dataCards.map(({ id, contact, description, icon }) => {
-                                        return (
-                                            <Box className={styles.contactContent} key={id} >
-                                                <Box className={styles.iconBox}>
-                                                    {icon}
+        <Box className={styles.contactContainer}>
+            <Container sx={{ py: { xs: 4, md: 8 } }}>
+                <Stack spacing={5} >
+                    <Typography variant="h3" sx={{ textAlign: 'center' }}>
+                        Get in touch with us
+                    </Typography>
+                    <Grid container spacing={2} sx={{
+                        bgcolor: '#F0F2FE',
+                        borderRadius: '24px',
+                        padding: { xs: 1, sm: 5 }
+                    }}>
+                        <Grid item xs={12} md={6}>
+                            <ContactForm />
+                        </Grid>
+                        <Grid item xs={12} md={6} >
+                            <Stack justifyContent={'center'} alignItems={'center'} spacing={2}
+                                sx={{ height: '100%', p: 1 }}>
+                                <Stack spacing={2} className={styles.inputsBox}>
+                                    {
+                                        dataCards.map(({ id, contact, description, icon }) => {
+                                            return (
+                                                <Box className={styles.contactContent} key={id} >
+                                                    <Box className={styles.iconBox}>
+                                                        {icon}
+                                                    </Box>
+                                                    <Box>
+                                                        <Typography variant='h6' sx={{ fontWeight: 600 }}>
+                                                            {contact}
+                                                        </Typography>
+                                                        <Typography color='text.secondary'>
+                                                            {description}
+                                                        </Typography>
+                                                    </Box>
                                                 </Box>
-                                                <Box>
-                                                    <Typography variant='h6' sx={{ fontWeight: 600 }}>
-                                                        {contact}
-                                                    </Typography>
-                                                    <Typography color='text.secondary'>
-                                                        {description}
-                                                    </Typography>
-                                                </Box>
-                                            </Box>
-                                        )
-                                    })}
+                                            )
+                                        })}
 
+                                </Stack>
                             </Stack>
-                        </Stack>
+                        </Grid>
                     </Grid>
-                </Grid>
-            </Stack>
-        </Container >
+                </Stack>
+            </Container >
+        </Box>
     )
 }
 
--- src/components/HomeComps/DiscoverMoreH/index.jsx
+++ src/components/HomeComps/DiscoverMoreH/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import styles from './index.module.css'
 import { Container, Box, Grid, Stack, Typography, Paper } from '@mui/material'
-import SignalCellularAltIcon from '@mui/icons-material/SignalCellularAlt';
 import { discoverData } from './DiscoverData';
 
 const DiscoverMore = () => {
@@ -11,10 +10,11 @@ const DiscoverMore = () => {
                 <Grid item xs={12} md={6}>
                     <Stack spacing={3}>
                         <Typography variant="h6" sx={{
-                            fontWeight: 600, backgroundImage: 'linear-gradient(to right, #373FFF, #3ACAF8, white, white)',
-                            WebkitBackgroundClip: 'text', color: ' transparent',  backgroundClip: 'text'
+                            fontWeight: 600,
+                            backgroundImage: 'linear-gradient(to right, #373FFF, #3ACAF8, white, white)',
+                            WebkitBackgroundClip: 'text', color: ' transparent', backgroundClip: 'text'
                         }}>Discover More</Typography>
-                        <Typography variant="h3" sx={{ fontSize: { xs: '2rem', md: '2.5rem', md: '3rem' } }} >Analyze your sales and marketing leads</Typography>
+                        <Typography variant="h3" sx={{ fontSize: { xs: '2rem', sm: '2.5rem', md: '3rem' } }} >Analyze your sales and marketing leads</Typography>
                         <Typography variant="body1">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua invidunt ut labore.</Typography>
                     </Stack>
                 </Grid>
@@ -27,8 +27,12 @@ const DiscoverMore = () => {
                                         {icon}
                                     </Box>
                                     <Box>
-                                        <Typography variant="h6" sx={{ fontWeight: 600 }}>{title}</Typography>
-                                        <Typography variant="body1">{body}</Typography>
+                                        <Typography variant="h6" sx={{ fontWeight: 600 }}>
+                                            {title}
+                                        </Typography>
+                                        <Typography variant="body1">
+                                            {body}
+                                        </Typography>
                                     </Box>
                                 </Box>
                             </Paper>
