769a8f0d920ea30da9eacf1807a2915fda8f6aa4
Gahramanoval301 <lemangahramanoval@gmail.com>
2023-12-24 11:30:42 +0300

    corrections

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import WebRouting from './routing'
-import RemoteTeams from './components/ProductComps/RemoteTeams'
+import FAQ from './components/PricingComps/FAQ'
+import ContactSection from './components/ContactComps/ContactSection'
 
 function App() {
 
   return (
-    // <RemoteTeams />
-    <WebRouting />
+    <ContactSection />
+    // <WebRouting />
 
   )
 }
