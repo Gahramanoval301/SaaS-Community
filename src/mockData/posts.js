c56444dc3e1cc9eeb02b8edbe2838fd386398c90
Gahramanoval301 <lemangahramanoval@gmail.com>
2023-12-25 17:58:50 +0300

    mock data of all posts

---
+++ src/mockData/posts.js
@@ -0,0 +1,91 @@
+export const posts = [{
+    id: 0,
+    title: 'New invoicing features to help you get paid faster',
+    about: 'Over the past few months, we’ve added several new features to SaaS Invoicing to help any business get paid faster and streamline their collection workflows.',
+    owner: 'Luke Matthews',
+    image: '/BlogSources/close-up-photography-of-woman-sitting-beside-table-while-using-macbook-3178818.png',
+    date: 'November 8, 2021',
+    slug:'new-invoicing-features-to-help-you-get-paid-faster',
+    isTrend: true
+}, {
+    id: 1,
+    title: '7 Automation use cases that foster excellent CX',
+    about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
+    owner: 'Luke Matthews',
+    image: '/BlogSources/people-on-a-video-call-4226122.png',
+    date: 'November 8, 2021',
+    slug:'7-automation-use-cases-that-foster-excellent-cx',
+    isTrend: false
+}, {
+    id: 2,
+    title: 'Managing Rails application secrets with encrypted credentials',
+    about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
+    owner: 'Luke Matthews',
+    image: '/BlogSources/people-on-a-video-call-4226262.png',
+    date: 'November 8, 2021',
+    slug:'managing-rails-application-secrets-with-encrypted-credentials',
+    isTrend: false
+}, {
+    id: 3,
+    title: 'Account-based marketing: Data-driven selection for ABM success',
+    about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
+    owner: 'Luke Matthews',
+    image: '/BlogSources/people-working-in-front-of-the-computer-3184357.png',
+    date: 'November 8, 2021',
+    slug:'account-based-marketing-data-driven-selection-for-abm-success',
+    isTrend: false
+}, {
+    id: 4,
+    title: 'How to Calculate Your SaaS Gross Margin',
+    about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
+    owner: 'Luke Matthews',
+    image: '/BlogSources/photo-of-boy-using-vr-headset-4145356.png',
+    date: 'November 8, 2021',
+    slug:'how-to-calculate-your-saas-gross-margin',
+    isTrend: false
+}, {
+    id: 5,
+    title: 'The Five Main Benefits of Automating your Accounts Receivable Process',
+    about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
+    owner: 'Luke Matthews',
+    image: '/BlogSources/photo-of-female-engineer-working-on-her-workspace-3862632.png',
+    date: 'November 8, 2021',
+    slug:'the-five-main-benefits-of-automating-your-accounts-receivable-process',
+    isTrend: false
+}, {
+    id: 6,
+    title: 'How to Make Data-driven Pricing Decisions',
+    about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
+    owner: 'Luke Matthews',
+    image: '/BlogSources/photo-of-woman-sitting-by-the-table-while-writing-4348078.png',
+    date: 'November 8, 2021',
+    slug:'how-to-make-data-driven-pricing-decisions',
+    isTrend: false
+}, {
+    id: 7,
+    title: 'How to Make Recurring Invoicing More Efficient',
+    about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
+    owner: 'Luke Matthews',
+    image: '/BlogSources/photo-of-woman-writing-on-tablet-computer-while-using-laptop-4348401.png',
+    date: 'November 8, 2021',
+    slug:'how-to-make-recurring-invoicing-more-efficient',
+    isTrend: false
+}, {
+    id: 8,
+    title: 'How to Reduce Checkout Abandonment for more Conversions',
+    about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
+    owner: 'Luke Matthews',
+    image: '/BlogSources/woman-having-video-call-in-home-office-4240610.png',
+    date: 'November 8, 2021',
+    slug:'how-to-reduce-checkout-abandonment-for-more-conversions',
+    isTrend: false
+}, {
+    id: 9,
+    title: '10 Customer Retention Strategies for better Dwell time',
+    about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
+    owner: 'Luke Matthews',
+    image: '/BlogSources/woman-writing-on-her-notebook-while-holding-a-cup-7616800.png',
+    date: 'November 8, 2021',
+    slug:'10-customer-retention-strategies-for-better-dwell-time',
+    isTrend: false
+}]
