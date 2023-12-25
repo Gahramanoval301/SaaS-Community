e95a7b24adddca016dc0be198bd7ccc410e8ce04
Gahramanoval301 <lemangahramanoval@gmail.com>
2023-12-25 20:19:54 +0300

    deleting of unused imports in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import WebRouting from './routing'
-import FAQ from './components/PricingComps/FAQ'
-import ContactSection from './components/ContactComps/ContactSection'
 
 function App() {
 
   return (
-    <ContactSection />
-    // <WebRouting />
-
+    <WebRouting />
   )
 }
 
